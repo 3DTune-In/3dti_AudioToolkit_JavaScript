d21290546fa16cd3aff9a82954cc247af3acca8e
Alexander Wallin <office@alexanderwallin.com>
2016-11-11 17:04:09 +0000

    Binds the (hopefully) minimal API necessary to spatialize web audio

--- HRTFFactory.cpp
+++ HRTFFactory.cpp
@@ -3,7 +3,12 @@
 #include <vector>
 #include <assert.h>
 #include <emscripten/bind.h>
+#include "3DTI_Toolkit_Core/Common/Buffer.h"
+#include "3DTI_Toolkit_Core/Common/Quaternion.h"
+#include "3DTI_Toolkit_Core/Common/Transform.h"
 #include "3DTI_Toolkit_Core/BinauralSpatializer/HRTF.h"
+#include "3DTI_Toolkit_Core/BinauralSpatializer/Listener.h"
+#include "3DTI_Toolkit_Core/BinauralSpatializer/SingleSourceDSP.h"
 
 using namespace emscripten;
 
@@ -74,7 +79,66 @@ EMSCRIPTEN_BINDINGS(HRTFModule) {
     .class_function("create", &HRTFFactory::create)
     ;
 
+  // Buffers
+  class_<CMonoBuffer<unsigned int>>("CMonoBuffer");
+  class_<CStereoBuffer<unsigned int>>("CStereoBuffer");
+
+  // CHRTF
   class_<CHRTF>("CHRTF")
+    ;
+
+  // CVector3
+  class_<CVector3>("CVector3")
+    .constructor<float, float, float>()
+    .property("x", &CVector3::x)
+    .property("y", &CVector3::y)
+    .property("z", &CVector3::z)
+    ;
+
+  // CQuaternion
+  class_<CQuaternion>("CQuaternion")
+    .class_function("FromAxisAngle", &CQuaternion::FromAxisAngle)
+    .constructor<float, float, float, float>()
+    // ToAxisAngle takes a float reference argument, which is a bit tricky.
+    // One solution is to wrap it in a proxy function.
+    // @link https://github.com/kripken/emscripten/issues/611
+    // .function("toAxisAngle", &CQuaternion::ToAxisAngle)
+    ;
+
+  // CTransform
+  class_<CTransform>("CTransform")
+    .constructor<>()
+    .function("GetPosition", &CTransform::GetPosition)
+    .function("SetPosition", &CTransform::SetPosition)
+    .function("GetOrientation", &CTransform::GetOrientation)
+    .function("SetOrientation", &CTransform::SetOrientation)
+    ;
+
+  // CListener
+  class_<Binaural::CListener>("CListener")
+    .constructor<Binaural::CCore*, float>()
+    .smart_ptr<std::shared_ptr<Binaural::CListener>>("CListenerPtr")
+    .function("LoadHRTF", &Binaural::CListener::LoadHRTF)
+    .function("GetListenerTransform", &Binaural::CListener::GetListenerTransform)
+    .function("SetListenerTransform", &Binaural::CListener::SetListenerTransform)
+    ;
+
+  // CSingleSourceDSP
+  class_<Binaural::CSingleSourceDSP>("CSingleSourceDSP")
+    .function("SetInterpolation", &Binaural::CSingleSourceDSP::SetInterpolation)
+    .function("SetFrequencyConvolution", &Binaural::CSingleSourceDSP::SetFrequencyConvolution)
+    .function("GetSourceTransform", &Binaural::CSingleSourceDSP::GetSourceTransform)
+    .function("SetSourceTransform", &Binaural::CSingleSourceDSP::SetSourceTransform)
+    .function("ProcessAnechoic", select_overload<void(const Binaural::CListener &, const CMonoBuffer<float> &, CStereoBuffer<float> &)>(&Binaural::CSingleSourceDSP::ProcessAnechoic))
+    ;
+
+  // CCore
+  class_<Binaural::CCore>("CCore")
+    .constructor<>()
+    .function("CreateListener", &Binaural::CCore::CreateListener)
+    .function("RemoveListener", &Binaural::CCore::RemoveListener)
+    .function("CreateSingleSourceDSP", &Binaural::CCore::CreateSingleSourceDSP)
+    .function("RemoveSingleSourceDSP", &Binaural::CCore::RemoveSingleSourceDSP)
     ;
 }
 
