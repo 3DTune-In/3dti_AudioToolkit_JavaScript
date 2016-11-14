10226a00290c0baf006444489da591d8684214ca
Alexander Wallin <office@alexanderwallin.com>
2016-11-14 15:22:42 +0000

    Enables debugging output using SWITCH_ON_3DTI_DEBUGGER flag in compile.sh

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ emcc \
   -I 3DTI_Toolkit_Core \
   -o HRTFFactory.js \
   -D _3DTI_AXIS_CONVENTION_OPENFRAMEWORK \
+  -D SWITCH_ON_3DTI_DEBUGGER \
   HRTFFactory.cpp \
   3DTI_Toolkit_Core/Common/*.cpp \
   3DTI_Toolkit_Core/BinauralSpatializer/*.cpp
