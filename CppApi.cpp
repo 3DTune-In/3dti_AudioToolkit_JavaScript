144372c4085a1a76229d4dbb9680d0fc8eefea49
Alexander Wallin <office@alexanderwallin.com>
2016-11-28 18:33:18 +0000

    dev-cpp-wrapper-api dump

---
+++ CppApi.cpp
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <vector>
+#include <emscripten/bind.h>
+#include "3DTI_Toolkit_Core/Common/Buffer.h"
+#include "3DTI_Toolkit_Core/Common/Debugger.h"
+#include "3DTI_Toolkit_Core/Common/Quaternion.h"
+#include "3DTI_Toolkit_Core/Common/Transform.h"
+#include "3DTI_Toolkit_Core/BinauralSpatializer/HRTF.h"
+#include "3DTI_Toolkit_Core/BinauralSpatializer/Listener.h"
+#include "3DTI_Toolkit_Core/BinauralSpatializer/SingleSourceDSP.h"
+
+using namespace emscripten;
+
+/**
+ * Basically a wrapper around std::vector<float>
+ */
+class FloatList
+{
+public:
+  FloatList()
+  {
+    data = std::vector<float>();
+  }
+
+  FloatList(std::vector<float> data)
+    : data(data)
+  {}
+
+  ~FloatList() {}
+
+  int Size()
+  {
+    return data.size();
+  }
+
+  void Add(float value)
+  {
+    data.push_back(value);
+  }
+
+  void Set(int index, float value)
+  {
+    data[index] = value;
+  }
+
+  float Get(int index)
+  {
+    return data[index];
+  }
+
+private:
+  std::vector<float> data;
+};
+
+/**
+ * HRIR wrapper
+ */
+class HRIR
+{
+public:
+  HRIR(FloatList buffer, int azimuth, int elevation)
+    : buffer(buffer), azimuth(azimuth), elevation(elevation)
+  {}
+
+  FloatList buffer;
+  int azimuth;
+  int elevation;
+};
+
+/**
+ * Main API
+ */
+class BinauralAPI
+{
+public:
+  BinauralAPI()
+  {
+    core = Binaural::CCore();
+  }
+
+  ~BinauralAPI() {}
+
+  /**
+   * Returns a CSingleSourceDSP instance
+   */
+  shared_ptr<Binaural::CSingleSourceDSP> CreateSource()
+  {
+    return core.CreateSingleSourceDSP();
+  }
+
+  /**
+   * Returns a CHRTF instance populated with all the data from
+   * the provided HRIR instances.
+   */
+  CHRTF CreateHRTF(std::vector<HRIR> hrirs)
+  {
+    const int length = 512;
+
+    CHRTF hrtf;
+    hrtf.BeginSetup(hrirs.size(), length, 44100);
+
+    for ( int i = 0; i < hrirs.size(); ++i )
+    {
+      HRIR &h = hrirs[i];
+
+      // Create a HRIR struct
+      HRIR_struct hrir_value;
+      hrir_value.leftHRIR.resize(length);
+      hrir_value.rightHRIR.resize(length);
+
+      // For wav files the delay is incorporated in the HRIR, so the variable delay is zero
+      hrir_value.leftDelay  = 0;
+      hrir_value.rightDelay = 0;
+
+      for ( int j = 0; j < length; j++ )
+      {
+        hrir_value.leftHRIR[j] = h.buffer.Get(j);
+        hrir_value.rightHRIR[j] = h.buffer.Get(j + length);
+      }
+
+      hrtf.AddHRIR(h.azimuth, h.elevation, std::move(hrir_value));
+    }
+
+    hrtf.EndSetup();
+
+    return hrtf;
+  }
+
+  /**
+   * Returns a binaural listener
+   *
+   * @param  listenerHeadRadius [description]
+   * @return                    [description]
+   */
+  shared_ptr<Binaural::CListener> CreateListener(std::vector<HRIR> hrirs, float listenerHeadRadius = 0.0875f)
+  {
+    shared_ptr<Binaural::CListener> listener = core.CreateListener(listenerHeadRadius);
+
+    CHRTF myHead = CreateHRTF(hrirs);
+    listener->LoadHRTF(std::move(myHead));
+
+    return listener;
+  }
+
+  /**
+   * Spatializes `inputBuffer` being located at `source`, with the `listener`
+   * as reference.
+   *
+   * @param source      [description]
+   * @param listener    [description]
+   * @param inputBuffer [description]
+   */
+  FloatList Spatialize(Binaural::CListener & listener, Binaural::CSingleSourceDSP & source, FloatList & inputData)
+  {
+    CMonoBuffer<float> inputBuffer;
+    CStereoBuffer<float> outputBuffer;
+
+    for (int i = 0; i < inputData.Size(); i++)
+    {
+      inputBuffer.push_back(inputData.Get(i));
+    }
+
+    source.ProcessAnechoic(listener, inputBuffer, outputBuffer);
+
+    // for (auto v : outputBuffer)
+    // {
+    //   printf("v = %f \n", v);
+    // }
+
+    FloatList outputData(outputBuffer);
+    return outputData;
+  }
+
+private:
+  Binaural::CCore core;
+};
+
+// Bindings
+EMSCRIPTEN_BINDINGS(Toolkit) {
+
+  // List of HRIRs
+  register_vector<HRIR>("HRIRVector");
+
+  /**
+   * FloatList bindings
+   */
+  class_<FloatList>("FloatList")
+    .constructor<>()
+    .function("Size", &FloatList::Size)
+    .function("Add", &FloatList::Add)
+    .function("Get", &FloatList::Get);
+
+  /**
+   * HRIR wrapper bindings
+   */
+  class_<HRIR>("HRIR")
+    .constructor<FloatList, int, int>();
+
+  /**
+   * Binaural lib
+   */
+  class_<Binaural::CListener>("CListener")
+    .smart_ptr<std::shared_ptr<Binaural::CListener>>("CListener_ptr");
+
+  class_<Binaural::CSingleSourceDSP>("CSingleSourceDSP")
+    .smart_ptr<std::shared_ptr<Binaural::CSingleSourceDSP>>("CSingleSourceDSP_ptr")
+    .function("SetSourceTransform", &Binaural::CSingleSourceDSP::SetSourceTransform);
+
+  class_<CTransform>("CTransform")
+    .constructor<>()
+    .function("SetPosition", &CTransform::SetPosition);
+
+  class_<CVector3>("CVector3")
+    .constructor<float, float, float>();
+
+  /**
+   * BinauralAPI bindings
+   */
+  class_<BinauralAPI>("BinauralAPI")
+    .constructor<>()
+    .function("CreateSource", &BinauralAPI::CreateSource)
+    .function("CreateListener", &BinauralAPI::CreateListener)
+    .function("Spatialize", &BinauralAPI::Spatialize)
+    ;
+}
---
+++ cpp-api-example.js
@@ -0,0 +1,350 @@
+const {
+  VectorFloat,
+  FloatList,
+  CVector3,
+  CTransform,
+  HRIR,
+  HRIRVector,
+  BinauralAPI,
+} = window.Module
+
+const ctx = new AudioContext()
+
+const assetsUrl = '/assets'
+
+const hrirUrls = [
+  'IRC_1032_C_R0195_T000_P000.wav',
+  'IRC_1032_C_R0195_T000_P015.wav',
+  'IRC_1032_C_R0195_T000_P030.wav',
+  'IRC_1032_C_R0195_T000_P045.wav',
+  'IRC_1032_C_R0195_T000_P060.wav',
+  'IRC_1032_C_R0195_T000_P075.wav',
+  'IRC_1032_C_R0195_T000_P090.wav',
+  'IRC_1032_C_R0195_T000_P315.wav',
+  'IRC_1032_C_R0195_T000_P330.wav',
+  'IRC_1032_C_R0195_T000_P345.wav',
+  'IRC_1032_C_R0195_T015_P000.wav',
+  'IRC_1032_C_R0195_T015_P015.wav',
+  'IRC_1032_C_R0195_T015_P030.wav',
+  'IRC_1032_C_R0195_T015_P045.wav',
+  'IRC_1032_C_R0195_T015_P315.wav',
+  'IRC_1032_C_R0195_T015_P330.wav',
+  'IRC_1032_C_R0195_T015_P345.wav',
+  'IRC_1032_C_R0195_T030_P000.wav',
+  'IRC_1032_C_R0195_T030_P015.wav',
+  'IRC_1032_C_R0195_T030_P030.wav',
+  'IRC_1032_C_R0195_T030_P045.wav',
+  'IRC_1032_C_R0195_T030_P060.wav',
+  'IRC_1032_C_R0195_T030_P315.wav',
+  'IRC_1032_C_R0195_T030_P330.wav',
+  'IRC_1032_C_R0195_T030_P345.wav',
+  'IRC_1032_C_R0195_T045_P000.wav',
+  'IRC_1032_C_R0195_T045_P015.wav',
+  'IRC_1032_C_R0195_T045_P030.wav',
+  'IRC_1032_C_R0195_T045_P045.wav',
+  'IRC_1032_C_R0195_T045_P315.wav',
+  'IRC_1032_C_R0195_T045_P330.wav',
+  'IRC_1032_C_R0195_T045_P345.wav',
+  'IRC_1032_C_R0195_T060_P000.wav',
+  'IRC_1032_C_R0195_T060_P015.wav',
+  'IRC_1032_C_R0195_T060_P030.wav',
+  'IRC_1032_C_R0195_T060_P045.wav',
+  'IRC_1032_C_R0195_T060_P060.wav',
+  'IRC_1032_C_R0195_T060_P075.wav',
+  'IRC_1032_C_R0195_T060_P315.wav',
+  'IRC_1032_C_R0195_T060_P330.wav',
+  'IRC_1032_C_R0195_T060_P345.wav',
+  'IRC_1032_C_R0195_T075_P000.wav',
+  'IRC_1032_C_R0195_T075_P015.wav',
+  'IRC_1032_C_R0195_T075_P030.wav',
+  'IRC_1032_C_R0195_T075_P045.wav',
+  'IRC_1032_C_R0195_T075_P315.wav',
+  'IRC_1032_C_R0195_T075_P330.wav',
+  'IRC_1032_C_R0195_T075_P345.wav',
+  'IRC_1032_C_R0195_T090_P000.wav',
+  'IRC_1032_C_R0195_T090_P015.wav',
+  'IRC_1032_C_R0195_T090_P030.wav',
+  'IRC_1032_C_R0195_T090_P045.wav',
+  'IRC_1032_C_R0195_T090_P060.wav',
+  'IRC_1032_C_R0195_T090_P315.wav',
+  'IRC_1032_C_R0195_T090_P330.wav',
+  'IRC_1032_C_R0195_T090_P345.wav',
+  'IRC_1032_C_R0195_T105_P000.wav',
+  'IRC_1032_C_R0195_T105_P015.wav',
+  'IRC_1032_C_R0195_T105_P030.wav',
+  'IRC_1032_C_R0195_T105_P045.wav',
+  'IRC_1032_C_R0195_T105_P315.wav',
+  'IRC_1032_C_R0195_T105_P330.wav',
+  'IRC_1032_C_R0195_T105_P345.wav',
+  'IRC_1032_C_R0195_T120_P000.wav',
+  'IRC_1032_C_R0195_T120_P015.wav',
+  'IRC_1032_C_R0195_T120_P030.wav',
+  'IRC_1032_C_R0195_T120_P045.wav',
+  'IRC_1032_C_R0195_T120_P060.wav',
+  'IRC_1032_C_R0195_T120_P075.wav',
+  'IRC_1032_C_R0195_T120_P315.wav',
+  'IRC_1032_C_R0195_T120_P330.wav',
+  'IRC_1032_C_R0195_T120_P345.wav',
+  'IRC_1032_C_R0195_T135_P000.wav',
+  'IRC_1032_C_R0195_T135_P015.wav',
+  'IRC_1032_C_R0195_T135_P030.wav',
+  'IRC_1032_C_R0195_T135_P045.wav',
+  'IRC_1032_C_R0195_T135_P315.wav',
+  'IRC_1032_C_R0195_T135_P330.wav',
+  'IRC_1032_C_R0195_T135_P345.wav',
+  'IRC_1032_C_R0195_T150_P000.wav',
+  'IRC_1032_C_R0195_T150_P015.wav',
+  'IRC_1032_C_R0195_T150_P030.wav',
+  'IRC_1032_C_R0195_T150_P045.wav',
+  'IRC_1032_C_R0195_T150_P060.wav',
+  'IRC_1032_C_R0195_T150_P315.wav',
+  'IRC_1032_C_R0195_T150_P330.wav',
+  'IRC_1032_C_R0195_T150_P345.wav',
+  'IRC_1032_C_R0195_T165_P000.wav',
+  'IRC_1032_C_R0195_T165_P015.wav',
+  'IRC_1032_C_R0195_T165_P030.wav',
+  'IRC_1032_C_R0195_T165_P045.wav',
+  'IRC_1032_C_R0195_T165_P315.wav',
+  'IRC_1032_C_R0195_T165_P330.wav',
+  'IRC_1032_C_R0195_T165_P345.wav',
+  'IRC_1032_C_R0195_T180_P000.wav',
+  'IRC_1032_C_R0195_T180_P015.wav',
+  'IRC_1032_C_R0195_T180_P030.wav',
+  'IRC_1032_C_R0195_T180_P045.wav',
+  'IRC_1032_C_R0195_T180_P060.wav',
+  'IRC_1032_C_R0195_T180_P075.wav',
+  'IRC_1032_C_R0195_T180_P315.wav',
+  'IRC_1032_C_R0195_T180_P330.wav',
+  'IRC_1032_C_R0195_T180_P345.wav',
+  'IRC_1032_C_R0195_T195_P000.wav',
+  'IRC_1032_C_R0195_T195_P015.wav',
+  'IRC_1032_C_R0195_T195_P030.wav',
+  'IRC_1032_C_R0195_T195_P045.wav',
+  'IRC_1032_C_R0195_T195_P315.wav',
+  'IRC_1032_C_R0195_T195_P330.wav',
+  'IRC_1032_C_R0195_T195_P345.wav',
+  'IRC_1032_C_R0195_T210_P000.wav',
+  'IRC_1032_C_R0195_T210_P015.wav',
+  'IRC_1032_C_R0195_T210_P030.wav',
+  'IRC_1032_C_R0195_T210_P045.wav',
+  'IRC_1032_C_R0195_T210_P060.wav',
+  'IRC_1032_C_R0195_T210_P315.wav',
+  'IRC_1032_C_R0195_T210_P330.wav',
+  'IRC_1032_C_R0195_T210_P345.wav',
+  'IRC_1032_C_R0195_T225_P000.wav',
+  'IRC_1032_C_R0195_T225_P015.wav',
+  'IRC_1032_C_R0195_T225_P030.wav',
+  'IRC_1032_C_R0195_T225_P045.wav',
+  'IRC_1032_C_R0195_T225_P315.wav',
+  'IRC_1032_C_R0195_T225_P330.wav',
+  'IRC_1032_C_R0195_T225_P345.wav',
+  'IRC_1032_C_R0195_T240_P000.wav',
+  'IRC_1032_C_R0195_T240_P015.wav',
+  'IRC_1032_C_R0195_T240_P030.wav',
+  'IRC_1032_C_R0195_T240_P045.wav',
+  'IRC_1032_C_R0195_T240_P060.wav',
+  'IRC_1032_C_R0195_T240_P075.wav',
+  'IRC_1032_C_R0195_T240_P315.wav',
+  'IRC_1032_C_R0195_T240_P330.wav',
+  'IRC_1032_C_R0195_T240_P345.wav',
+  'IRC_1032_C_R0195_T255_P000.wav',
+  'IRC_1032_C_R0195_T255_P015.wav',
+  'IRC_1032_C_R0195_T255_P030.wav',
+  'IRC_1032_C_R0195_T255_P045.wav',
+  'IRC_1032_C_R0195_T255_P315.wav',
+  'IRC_1032_C_R0195_T255_P330.wav',
+  'IRC_1032_C_R0195_T255_P345.wav',
+  'IRC_1032_C_R0195_T270_P000.wav',
+  'IRC_1032_C_R0195_T270_P015.wav',
+  'IRC_1032_C_R0195_T270_P030.wav',
+  'IRC_1032_C_R0195_T270_P045.wav',
+  'IRC_1032_C_R0195_T270_P060.wav',
+  'IRC_1032_C_R0195_T270_P315.wav',
+  'IRC_1032_C_R0195_T270_P330.wav',
+  'IRC_1032_C_R0195_T270_P345.wav',
+  'IRC_1032_C_R0195_T285_P000.wav',
+  'IRC_1032_C_R0195_T285_P015.wav',
+  'IRC_1032_C_R0195_T285_P030.wav',
+  'IRC_1032_C_R0195_T285_P045.wav',
+  'IRC_1032_C_R0195_T285_P315.wav',
+  'IRC_1032_C_R0195_T285_P330.wav',
+  'IRC_1032_C_R0195_T285_P345.wav',
+  'IRC_1032_C_R0195_T300_P000.wav',
+  'IRC_1032_C_R0195_T300_P015.wav',
+  'IRC_1032_C_R0195_T300_P030.wav',
+  'IRC_1032_C_R0195_T300_P045.wav',
+  'IRC_1032_C_R0195_T300_P060.wav',
+  'IRC_1032_C_R0195_T300_P075.wav',
+  'IRC_1032_C_R0195_T300_P315.wav',
+  'IRC_1032_C_R0195_T300_P330.wav',
+  'IRC_1032_C_R0195_T300_P345.wav',
+  'IRC_1032_C_R0195_T315_P000.wav',
+  'IRC_1032_C_R0195_T315_P015.wav',
+  'IRC_1032_C_R0195_T315_P030.wav',
+  'IRC_1032_C_R0195_T315_P045.wav',
+  'IRC_1032_C_R0195_T315_P315.wav',
+  'IRC_1032_C_R0195_T315_P330.wav',
+  'IRC_1032_C_R0195_T315_P345.wav',
+  'IRC_1032_C_R0195_T330_P000.wav',
+  'IRC_1032_C_R0195_T330_P015.wav',
+  'IRC_1032_C_R0195_T330_P030.wav',
+  'IRC_1032_C_R0195_T330_P045.wav',
+  'IRC_1032_C_R0195_T330_P060.wav',
+  'IRC_1032_C_R0195_T330_P315.wav',
+  'IRC_1032_C_R0195_T330_P330.wav',
+  'IRC_1032_C_R0195_T330_P345.wav',
+  'IRC_1032_C_R0195_T345_P000.wav',
+  'IRC_1032_C_R0195_T345_P015.wav',
+  'IRC_1032_C_R0195_T345_P030.wav',
+  'IRC_1032_C_R0195_T345_P045.wav',
+  'IRC_1032_C_R0195_T345_P315.wav',
+  'IRC_1032_C_R0195_T345_P330.wav',
+  'IRC_1032_C_R0195_T345_P345.wav',
+].map(filename => `${assetsUrl}/${filename}`)
+
+/**
+ *
+ */
+const getFloatListWithFloats = floats => {
+
+}
+
+/**
+ * Returns an object with azimuth and elevation angles extracted
+ * from a URL.
+ */
+const getAnglesFromUrl = url => {
+  const [, azimuth, elevation] = url.split(/.+T(\d{3})_P(\d{3})/).map(x => parseInt(x))
+  return { azimuth, elevation }
+}
+
+/**
+ * Fetches a wav file and returns it as an array buffer.
+ */
+const fetchWavFile = url => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.responseType = 'arraybuffer'
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        resolve(xhr.response)
+      }
+    }
+    xhr.send()
+  })
+}
+
+/**
+ * Returns a Promise resolving with an AudioBuffer decoded from
+ * an ArrayBuffer.
+ */
+const decodeBuffer = buffer => {
+  return ctx.decodeAudioData(buffer)
+}
+
+/**
+ * Returns an array of HRIR-looking objects from a set of HRIR
+ * wav URLs
+ */
+const fetchWavFiles = urls => {
+  return Promise.all(urls.map(fetchWavFile))
+    .then(buffers => Promise.all(buffers.map(decodeBuffer)))
+    .then(buffers => {
+      return buffers.map((x, i) => {
+        return Object.assign(
+          {},
+          getAnglesFromUrl(urls[i]),
+          { buffer: x }
+        )
+      })
+    })
+}
+
+let first = true
+let stop = false
+window.addEventListener('click', () => stop = !stop)
+
+const api = new BinauralAPI()
+
+// Just do it
+fetchWavFiles(hrirUrls)
+  .then(results => {
+    const hrirs = results.map(({ buffer, azimuth, elevation }) => {
+      data = new FloatList()
+      for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
+        for (let i = 0; i < buffer.length; i++) {
+          data.Add(buffer.getChannelData(channel)[i])
+        }
+      }
+
+      return new HRIR(data, azimuth, elevation)
+    })
+
+    const hrirsVector = new HRIRVector()
+    for (const hrir of hrirs) {
+      hrirsVector.push_back(hrir)
+    }
+
+    console.log({ hrirs, hrirsVector })
+
+    const listener = api.CreateListener(hrirsVector, 0.09)
+    console.log({ listener })
+
+    const source = api.CreateSource()
+    console.log({ source })
+
+    const sourceTransform = new CTransform()
+    sourceTransform.SetPosition(new CVector3(-20, 0, -10))
+    source.SetSourceTransform(sourceTransform)
+
+    // Synth
+    const oscillator = ctx.createOscillator()
+    oscillator.frequency.value = 440
+    const gain = ctx.createGain()
+    gain.gain.value = 0.3
+
+    // Script processor
+    const scriptNode = ctx.createScriptProcessor(512, 2, 2)
+    scriptNode.onaudioprocess = (audioProcessingEvent) => {
+      if (stop) {
+        return
+      }
+
+      const { inputBuffer, outputBuffer } = audioProcessingEvent
+
+      const inputData = inputBuffer.getChannelData(0)
+      const inputMonoBuffer = new FloatList()
+      for (let i = 0; i < inputData.length; i++) {
+        inputMonoBuffer.Add(inputData[i])
+      }
+
+      try {
+        const outputFloats = api.Spatialize(listener, source, inputMonoBuffer)
+
+        const outputDataLeft = outputBuffer.getChannelData(0)
+        const outputDataRight = outputBuffer.getChannelData(1)
+
+        for (let i = 0; i < outputFloats.Size(); i += 2) {
+          outputDataLeft[i] = outputFloats.Get(i)
+          outputDataLeft[i + 1] = outputFloats.Get(i + 1)
+        }
+      }
+      catch (ex) {
+        console.log('execption:', ex)
+      }
+
+      if (first) {
+        stop = true
+        first = false
+      }
+    }
+
+    oscillator.connect(scriptNode)
+    scriptNode.connect(gain)
+    gain.connect(ctx.destination)
+
+    oscillator.start(0)
+  })
+  .catch(err => console.log({ err }))
+
