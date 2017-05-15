0b7ee394ade4f1c3f4c8eacffc0e46e104007de2
Alexander Wallin <office@alexanderwallin.com>
2017-05-15 17:34:15 +0100

    Specifies Web Audio API axis convention in all compile scripts

--- compile-hls-debug.bat
+++ compile-hls-debug.bat
@@ -3,7 +3,7 @@ emcc ^
   --bind ^
   -I 3DTI_Toolkit_Core ^
   -o build/3dti-toolkit-hls.js ^
-  -D _3DTI_AXIS_CONVENTION_OPENFRAMEWORK ^
+  -D _3DTI_AXIS_CONVENTION_WEBAUDIOAPI ^
   -D SWITCH_ON_3DTI_DEBUGGER ^
   -g4 ^
   -s DEMANGLE_SUPPORT=1 ^
